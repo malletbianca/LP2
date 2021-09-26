e4e74d5b295eb305efa58fc1e1523ca1c9f34fda
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-09-26 19:03:46 -0300

    Reestrutura a classe de Figura

--- EditorGrafico/figures/Figure.java
+++ EditorGrafico/figures/Figure.java
@@ -3,11 +3,21 @@ package figures;
 import java.awt.Graphics;
 
 public abstract class Figure {
-    int x, y;
-    int rot;
-    int rFill, gFill, bFill;
-    int stroke;
-    int rStroke, gStroke, bStroke;
+    public int x, y;
+    public int rot;
+    public int stroke;
+    public int rStroke, gStroke, bStroke;
+
+    public Figure (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke) {
+    	this.x = x;
+        this.y = y;
+        this.rot = rot;
+        this.stroke = stroke;
+        this.rStroke = rStroke;
+        this.gStroke = gStroke;
+        this.bStroke = bStroke;
+    }
 
     public abstract void paint (Graphics g);
+    
 }
