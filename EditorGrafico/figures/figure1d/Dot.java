76553431a23336c3a1ec50a7a1ac66baf34a113a
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-10-03 15:45:20 -0300

    Adapta os métodos para a figura de ponto

--- EditorGrafico/figures/figure1d/Dot.java
+++ EditorGrafico/figures/figure1d/Dot.java
@@ -6,14 +6,41 @@ import java.awt.Graphics;
 
 public class Dot extends Figure {
 
-    public Dot (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke) {
-        super(x,y, rot, stroke, rStroke, gStroke, bStroke);
+    public Dot (int x, int y, int stroke, int rStroke, int gStroke, int bStroke) {
+        super(x,y, stroke, rStroke, gStroke, bStroke);
+        this.stroke = 8;
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
         g2d.setColor(new Color(this.rStroke, this.gStroke, this.bStroke));
         g2d.fillOval(this.x - (this.stroke/2),this.y - (this.stroke/2), this.stroke,this.stroke);
+    }
+
+    public boolean clicked (int mouseX, int mouseY) {
+        return (mouseX >= this.x - (this.stroke/2)) && (mouseX <= this.x + (this.stroke/2)) && (mouseY >= this.y - (this.stroke/2)) && (mouseY <= this.y + (this.stroke/2));
+    }
+
+    public void randomColor (int r, int g, int b) {
+        this.rStroke = r;
+        this.gStroke = g;
+        this.bStroke = b;
+    }
+
+    public void scale (int scaleWidth, int scaleHeight) {
+        this.stroke += scaleWidth;
+
+        // Evita que a figura seja reduzida indefinidamente
+        if (this.stroke < 3) {
+            this.stroke -= scaleWidth;
+        }
+    }
+
+    public void addSelection(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(3));
+        g2d.setColor(Color.RED);
+        g2d.drawOval(this.x - (this.stroke/2),this.y - (this.stroke/2), this.stroke,this.stroke);
+
     }
 }
