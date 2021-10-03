376b7fd47c01be0033782aa2b7617af219e8ab58
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-10-03 16:01:35 -0300

    Ajusta figura e edita os métodos

--- EditorGrafico/figures/figure2d/Triangle.java
+++ EditorGrafico/figures/figure2d/Triangle.java
@@ -2,36 +2,56 @@ package figures.figure2d;
 
 import figures.*;
 import java.awt.*;
+import java.awt.Polygon;
+import java.awt.Graphics;
 
 public class Triangle extends Figure2D {
-    int numberOfSides = 3;
-    int [] x = new int[numberOfSides];
-    int [] y = new int[numberOfSides];
+    Polygon triangle;
     
-    public Triangle (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke, int width, int height, int rFill, int gFill, int bFill) {
-        super(x,y, rot, stroke, rStroke, gStroke, bStroke, width, height, rFill, gFill, bFill);
+    public Triangle (int x, int y, int stroke, int rStroke, int gStroke, int bStroke, int width, int height, int rFill, int gFill, int bFill) {
+        super(x,y, stroke, rStroke, gStroke, bStroke, width, height, rFill, gFill, bFill);
 
-        // Constroi triângulos equiláteros e isósceles
-        this.x[0] = x + (width/2);
-        this.x[1] = width + x;
-        this.x[2] = x;
-
-        this.y[0] = y;
-        this.y[1] = height + y;
-        this.y[2] = height + y;
+        triangle = new Polygon();
+        triangle.xpoints = new int[] {this.x + (this.width/2), this.width + this.x, this.x};
+        triangle.ypoints = new int[] {this.y, (this.height + this.y), (this.height + this.y)};
+        triangle.npoints = 3;
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(new Color(this.rFill, this.gFill, this.bFill));
-        g2d.fillPolygon(this.x, this.y, 3);
+        g2d.fillPolygon(triangle);
 
         if (stroke > 0) {
             g2d.setColor(new Color(this.rStroke, this.gStroke, this.bStroke));
             g2d.setStroke(new BasicStroke(this.stroke, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-            g2d.drawPolygon(this.x, this.y, 3);
+            g2d.drawPolygon(triangle);
         }
+    }
 
+    public void drag (int distanceX, int distanceY) {
+        this.x += distanceX;
+        this.y += distanceY;
+
+        for (int i = 0; i < triangle.npoints; ++i) {
+            triangle.xpoints[i] += distanceX;
+            triangle.ypoints[i] += distanceY;
+        }
+    }
+
+    public void scale (int scaleWidth, int scaleHeight) {
+        this.width += scaleWidth;
+        this.height += scaleHeight;
+
+        triangle.xpoints = new int[] {this.x + (this.width/2), this.width + this.x, this.x};
+        triangle.ypoints = new int[] {this.y, (this.height + this.y), (this.height + this.y)};
+    }
+
+    public void addSelection(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(3));
+        g2d.setColor(Color.RED);
+        g2d.drawPolygon(triangle);
     }
 }
