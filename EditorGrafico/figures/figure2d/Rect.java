77238d50c223cee562de039b67f11a4d86b1e17d
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-10-03 15:53:37 -0300

    Ajusta figura e adiciona método de seleção ativa

--- EditorGrafico/figures/figure2d/Rect.java
+++ EditorGrafico/figures/figure2d/Rect.java
@@ -6,13 +6,12 @@ import java.awt.Graphics;
 
 public class Rect extends Figure2D {
 
-    public Rect (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke, int width, int height, int rFill, int gFill, int bFill) {
-        super(x,y, rot, stroke, rStroke, gStroke, bStroke, width, height, rFill, gFill, bFill);
+    public Rect (int x, int y, int width, int height, int stroke, int rStroke, int gStroke, int bStroke, int rFill, int gFill, int bFill) {
+        super(x,y, width,height, stroke, rStroke, gStroke, bStroke, rFill, gFill, bFill);
     }
 
     public void paint (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-
         g2d.setColor(new Color(this.rFill, this.gFill, this.bFill));
         g2d.fillRect(this.x,this.y, this.width,this.height);
 
@@ -21,5 +20,13 @@ public class Rect extends Figure2D {
             g2d.setStroke(new BasicStroke(this.stroke));
             g2d.drawRect(this.x,this.y, this.width,this.height);
         }
+    }
+
+    public void addSelection(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(3));
+        g2d.setColor(Color.RED);
+        g2d.drawRect(this.x,this.y, this.width,this.height);
+
     }
 }
