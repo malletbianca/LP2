a4ddf7ef339352cdfaa07e5cfc154dfb5bb49b55
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-10-03 15:42:46 -0300

    Adiciona os métodos à classe de Figura

--- EditorGrafico/figures/Figure.java
+++ EditorGrafico/figures/Figure.java
@@ -4,20 +4,31 @@ import java.awt.Graphics;
 
 public abstract class Figure {
     public int x, y;
-    public int rot;
     public int stroke;
     public int rStroke, gStroke, bStroke;
 
-    public Figure (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke) {
+    public Figure (int x, int y, int stroke, int rStroke, int gStroke, int bStroke) {
     	this.x = x;
-        this.y = y;
-        this.rot = rot;
+        this.y = y; 
         this.stroke = stroke;
         this.rStroke = rStroke;
         this.gStroke = gStroke;
         this.bStroke = bStroke;
     }
 
+    public void drag (int distanceX, int distanceY) {
+        this.x += distanceX;
+        this.y += distanceY;
+    }
+
+    public abstract boolean clicked (int mouseX, int mouseY);
+    
+    public abstract void scale (int scaleWidth, int scaleHeight);
+
     public abstract void paint (Graphics g);
+
+    public abstract void addSelection(Graphics g);
+
+    public abstract void randomColor (int r, int g, int b);
     
 }
