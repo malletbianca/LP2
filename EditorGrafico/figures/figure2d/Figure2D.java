58e86efe32df425b8c748a46a2aff4b98c77f0a2
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-10-03 15:48:21 -0300

    Adapta os métodos para as figuras 2d

--- EditorGrafico/figures/figure2d/Figure2D.java
+++ EditorGrafico/figures/figure2d/Figure2D.java
@@ -4,15 +4,36 @@ import figures.*;
 import java.awt.Graphics;
 
 public abstract class Figure2D extends Figure {
-	public int width, height;
+    public int width, height;
     public int rFill, gFill, bFill;
 
-    public Figure2D (int x, int y, int rot, int stroke, int rStroke, int gStroke, int bStroke, int width, int height, int rFill, int gFill, int bFill) {
-    	super(x,y, rot, stroke, rStroke, gStroke, bStroke);
-    	this.width = width;
-    	this.height = height;
+    public Figure2D (int x, int y, int width, int height, int stroke, int rStroke, int gStroke, int bStroke, int rFill, int gFill, int bFill) {
+    	super(x,y, stroke, rStroke, gStroke, bStroke);
+        this.width = width;
+        this.height = height;
     	this.rFill = rFill;
     	this.gFill = gFill;
     	this.bFill = bFill;
+    }
+
+    public boolean clicked (int mouseX, int mouseY) {
+        return (mouseX >= this.x) && (mouseX <= this.x + this.width) && (mouseY >= this.y) && (mouseY <= this.y + this.height);
+    }
+
+    public void scale (int scaleWidth, int scaleHeight) {
+        this.width += scaleWidth;
+        this.height += scaleHeight;
+
+        // Evita que a figura seja reduzida indefinidamente
+        if (this.width < 5) {
+            this.width -= scaleWidth;
+            this.height -= scaleHeight;
+        }
+    }
+
+    public void randomColor (int r, int g, int b) {
+        this.rFill = r;
+        this.gFill = g;
+        this.bFill = b;
     }
 }
