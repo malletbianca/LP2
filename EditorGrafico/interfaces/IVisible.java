193d6c3986b5c2364784d168ff3b5e20fdbfee2c
malletbianca <38991580+malletbianca@users.noreply.github.com>
2021-11-07 17:37:11 -0300

    Adiciona parâmetro boolean ao método paint

--- EditorGrafico/interfaces/IVisible.java
+++ EditorGrafico/interfaces/IVisible.java
@@ -4,5 +4,5 @@ import java.awt.*;
 
 public interface IVisible {
 	public boolean clicked (int mouseX, int mouseY);
-	public void paint (Graphics g);
+	public void paint (Graphics g, boolean focused);
 }
